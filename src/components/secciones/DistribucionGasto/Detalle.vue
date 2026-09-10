6ffc95395e56ca2fc0b89eba28b5813d3072b82d
Hector Jair Velazquez Jimenez <hector_velazquez@hacienda.gob.mx>
2026-09-10 13:02:51 -0600

    fix alineacion texto

--- src/components/secciones/DistribucionGasto/Detalle.vue
+++ src/components/secciones/DistribucionGasto/Detalle.vue
@@ -57,7 +57,7 @@ interface Segmento {
                 title="Variación">Variación</span>
             <span class="col-span-8 truncate px-2  text-[#0a5a45] bg-[#f5f2ed] font-bold"
                 title="Desagregado">Desagregado</span>
-            <span class="col-span-8 truncate px-2 text-[#0a5a45] text-center bg-[#f5f2ed] font-bold"
+            <span class="col-span-8 truncate px-2 text-[#0a5a45] text-end bg-[#f5f2ed] font-bold"
                 title="2027">2027</span>
         </div>
         <div class="hidden md:grid grid-cols-24 col-span-12 gap-0" v-for="(row, i) in detalle.segmentos">
@@ -68,7 +68,7 @@ interface Segmento {
                 '--color-flecha': row.tendencia === '▲' ? '#2a9d8f' : row.tendencia === '▼' ? '#e63946' : '#888888'
             }"></span>
             <span class="col-span-8 px-2  text-black truncate " v-html="row.ramo" :title="row.ramo"></span>
-            <span class="col-span-8 px-2 text-center text-black truncate" :title="row.monto" v-html="row.monto"></span>
+            <span class="col-span-8 px-2 text-end text-black truncate" :title="row.monto" v-html="row.monto"></span>
 
 
         </div>
